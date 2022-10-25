b0da77e15afacc7921914e96cef707df065f67df
Davi Ferreira <daviferreiraaew@gmail.com>
2022-10-25 09:31:42 -0300

    modularizing the error alert box

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 import {Modal} from './modal.js'
+import {AlertError} from './alert-error.js'
 
 //variables
 const form = document.querySelector('form')
@@ -13,9 +14,10 @@ form.onsubmit = function (event) {
 
   const showAlertError = notANumber(weight) || notANumber(height)
   if (showAlertError) {
-    console.log('mostrar alerta de erro')
+    AlertError.open()
     return;
   }
+  AlertError.close()
 
   const result = IMC(weight, height)
   const message = `Seu IMC é de ${result}`
